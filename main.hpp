4d3a8bc4505b852a11667716bf15f2bd83959207
Alan Montes <amontescastro@students.avc.edu>
2024-10-25 18:25:03 -0700

    Created Code

--- main.hpp
+++ main.hpp
@@ -1,8 +1,62 @@
+#include <iomanip>
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+using namespace std;
 
-//********************
-// Declare Function Prototypes
-//********************
+void getTwoValues(int &begin, int &end);
+int getNextPrime(int begin);
+int getPrevPrime(int end);
 
-// ******************************
-// Implement all your functions here
-// ******************************
+
+void getTwoValues(int &begin, int &end){
+    do
+    {
+    cout << "Enter two Values: ";
+    cin >> begin >> end;
+    } while (begin > end);
+    
+}
+
+int getNextPrime(int begin){
+int i,j;
+int flag;
+	for (i = begin; i <= 3000; i++){
+		flag = 1;
+		for (j = 2; j <= (i/2); ++j){
+                        
+			if ((i % j == 0)){
+			flag = 0;
+			break;
+			}
+                        
+			}
+		if (flag == 1){
+		return i;
+        break;
+        }
+    }
+    return 0;
+}
+
+int getPrevPrime(int end ){
+int i,j;
+int flag;
+	for (i = end; i >= 5; i--){
+		flag = 1;
+		for (j = 2; j <= (i/2); ++j){
+                        
+			if ((i % j == 0)){
+			flag = 0;
+			break;
+			}
+                        
+			}
+		if (flag == 1){
+		return i;
+        break;
+        }
+    }
+return 0;
+}
+
